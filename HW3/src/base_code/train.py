6c8c6d70b178dc76f9fdc146f6639d44c81169f7
Erick David Cohen <erickdcohen@gmail.com>
2025-03-25 13:28:53 -0400

    adds working base

---
+++ HW3/src/base_code/train.py
@@ -0,0 +1,10 @@
+from lightning import Trainer
+from datamodule import PAFDatamodule
+from prot_bert import ProteinClassifier
+
+if __name__ == "__main__":
+    n_classes = 25
+    datamodule = PAFDatamodule("data/datafiles", batch_size=32)
+    model = ProteinClassifier(n_classes=n_classes)
+    trainer = Trainer(max_epochs=10, accelerator="gpu", devices="auto")
+    trainer.fit(model=model, datamodule=datamodule)
---
+++ README.md
@@ -0,0 +1,3 @@
+# Deep Learning and Neural Networks in Science
+
+This repo serves to hold code written during the Deep Learning and Neural Networks course in Carnegie Mellon University Spring 2025
